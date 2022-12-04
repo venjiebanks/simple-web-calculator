d78fb25f860499ba44d4a6e7bc3c939d665fb775
venjie <esvbanks@gmail.com>
2022-12-03 20:46:09 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function display(val){
+
+    document.getElementById('result').value += val
+    return val
+
+}
+
+function solve(){
+
+    let a = document.getElementById('result').value
+    let b = eval(a);
+    document.getElementById('result').value = b;
+    return b
+
+}
+
+function clearScr(){
+
+    document.getElementById('result').value = ''
+
+}
